3f23826db8cfb019dd2caecd73373e6262a2ba7e
fengzhu92 <zhufengq@gmail.com>
2021-01-27 23:30:18 -0800

    initial checkin

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using TensorFlow;
+
+namespace CheckContamination
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string model = "model.pb";
+        public MainWindow ()
+        {
+            InitializeComponent ();
+        }
+
+        private void BrowseButton_Click ( object sender, RoutedEventArgs e )
+        {
+            Dispatcher.BeginInvoke ( new Action ( () =>
+            {
+                BitmapImage bitmap = new BitmapImage ();
+                bitmap.BeginInit ();
+                bitmap.UriSource = new Uri ( "pack://application:,,,/empty.jpg" );
+                bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                bitmap.CreateOptions = BitmapCreateOptions.IgnoreImageCache;
+                bitmap.EndInit ();
+                bitmap.Freeze ();
+                viewer.Source = bitmap;
+                viewer.UpdateLayout ();
+                imageTB.Clear ();
+                resultTB.Text = "";
+            } ), DispatcherPriority.ContextIdle, null );
+
+            OpenFileDialog openFileDialog = new OpenFileDialog ();
+            if ( openFileDialog.ShowDialog () == true )
+            {
+                imageTB.Text = openFileDialog.FileName;
+
+                BitmapImage bitmap = new BitmapImage ();
+                bitmap.BeginInit ();
+                bitmap.UriSource = new Uri ( imageTB.Text );
+                bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                bitmap.CreateOptions = BitmapCreateOptions.IgnoreImageCache;
+                bitmap.EndInit ();
+                bitmap.Freeze ();
+                viewer.Source = bitmap;
+                viewer.UpdateLayout ();
+            }   
+        }
+
+        private void Run_Click ( object sender, RoutedEventArgs e )
+        {
+            byte[] buffer = System.IO.File.ReadAllBytes ( model );
+            using ( var graph = new TensorFlow.TFGraph () )
+            {
+                graph.Import ( buffer );
+                using ( var session = new TensorFlow.TFSession ( graph ) )
+                {
+                    var file = imageTB.Text; 
+                    var runner = session.GetRunner ();
+                    graph.GetEnumerator ();
+                    var tensor = Utils.ImageToTensor ( file );
+                    runner.AddInput ( graph["img"][0], tensor );   
+                    runner.Fetch ( graph["dense_3/Softmax"][0] );
+
+                    var output = runner.Run ();
+                    var vecResults = output[0].GetValue ();
+                    float[,] results = (float[,]) vecResults;
+
+                    /// Evaluate the results
+                    int[] quantized = Utils.Quantized ( results );
+                    resultTB.Text = ( quantized[0] == 1 ) ? "Not Contaminated" : "Contaminated";
+                }
+            }
+           
+        }
+    }
+}
---
+++ Utils.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace CheckContamination
+{
+    class Utils
+    {
+        public static TensorFlow.TFTensor ImageToTensor ( string file )
+        {
+            using ( Bitmap image = (Bitmap) Image.FromFile ( file ) )
+            {
+                Bitmap smallerImage = ResizeImage ( image, 300, 300 );
+                var matrix = new float[1, smallerImage.Size.Height, smallerImage.Size.Width, 3];
+                for ( var iy = 0; iy < smallerImage.Size.Height; iy++ )
+                {
+                    for ( int ix = 0; ix < smallerImage.Size.Width; ix++ )
+                    {
+                        Color pixel = smallerImage.GetPixel ( ix, iy );
+                        matrix[0, iy, ix, 0] = pixel.B / 255.0f;
+                        matrix[0, iy, ix, 1] = pixel.G / 255.0f;
+                        matrix[0, iy, ix, 2] = pixel.R / 255.0f;
+                    }
+                }
+                TensorFlow.TFTensor tensor = matrix;
+                return tensor;
+            }
+        }
+
+        public static Bitmap ResizeImage ( Image image, int width, int height )
+        {
+            var destRect = new Rectangle ( 0, 0, width, height );
+            var destImage = new Bitmap ( width, height );
+
+            destImage.SetResolution ( image.HorizontalResolution, image.VerticalResolution );
+
+            using ( var graphics = Graphics.FromImage ( destImage ) )
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using ( var wrapMode = new ImageAttributes () )
+                {
+                    wrapMode.SetWrapMode ( WrapMode.TileFlipXY );
+                    graphics.DrawImage ( image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode );
+                }
+            }
+
+            return destImage;
+        }
+
+        //Silly repetitions here! I was running out of time.
+        internal static int[] Quantized ( float[,] results )
+        {
+            int[] q = new int[]
+            {
+                results[0,0]>0.5?1:0,
+                results[0,1]>0.5?1:0,
+            };
+            return q;
+        }
+    }
+}
